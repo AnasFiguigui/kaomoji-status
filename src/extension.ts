9ddf206788aa89f5757da506b7c6bfb78d2de8c7
AnasFiguigui <anas.production.af@gmail.com>
2026-02-14 17:00:48 +0100

    Added the kamojis to the status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,101 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let statusBarItem: vscode.StatusBarItem;
+let interval: NodeJS.Timeout | undefined;
+let isChanging = false; // Block spam clicks during manual change
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "kaomoji-status" is now active!');
+const kaomojis = [
+    "ψ(｀∇´)ψ",
+    "(。・ω・。)",
+    "(ง •̀_•́)ง",
+    "(づ｡◕‿‿◕｡)づ",
+    "(¬‿¬)",
+    "(☞ﾟヮﾟ)☞",
+    "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧",
+    "(╯°□°）╯︵ ┻━┻"
+];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('kaomoji-status.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from kaomoji-status!');
-	});
-
-	context.subscriptions.push(disposable);
+function getRandomKaomoji(): string {
+    return kaomojis[Math.floor(Math.random() * kaomojis.length)];
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+// Auto-change without spinner
+function changeKaomojiInstant() {
+    statusBarItem.text = getRandomKaomoji();
+}
+
+// Start the interval for auto-changing kaomoji
+function startInterval() {
+    const config = vscode.workspace.getConfiguration("kaomojiStatus");
+    const minutes = config.get<number>("interval", 15);
+
+    if (interval) {
+        clearInterval(interval);
+    }
+
+    interval = setInterval(() => {
+        changeKaomojiInstant(); // instant change, no spinner
+    }, minutes * 60 * 1000);
+}
+
+// Smooth change with spinner and click-block (manual click)
+async function changeKaomojiSmooth() {
+    if (isChanging) return; // Block spam clicks
+    isChanging = true;
+
+    // Show spinner for 300ms
+    statusBarItem.text = "$(sync~spin)";
+    statusBarItem.tooltip = "Changing...";
+
+    await new Promise(resolve => setTimeout(resolve, 200));
+
+    // Show new kaomoji
+    statusBarItem.text = getRandomKaomoji();
+    statusBarItem.tooltip = "Click to change kaomoji";
+
+    isChanging = false;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+    // Create status bar item on LEFT, end of left section
+    statusBarItem = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left,
+        -9999
+    );
+
+    statusBarItem.text = getRandomKaomoji();
+    statusBarItem.tooltip = "Click to change kaomoji";
+    statusBarItem.command = "kaomojiStatus.change";
+    statusBarItem.show();
+
+    // Start auto-change interval
+    startInterval();
+
+    // Command: change manually with smooth spinner
+    const changeCommand = vscode.commands.registerCommand(
+        "kaomojiStatus.change",
+        () => {
+            changeKaomojiSmooth();
+        }
+    );
+
+    // Listen for setting changes (live update interval)
+    const configListener = vscode.workspace.onDidChangeConfiguration(e => {
+        if (e.affectsConfiguration("kaomojiStatus.interval")) {
+            startInterval();
+        }
+    });
+
+    context.subscriptions.push(
+        statusBarItem,
+        changeCommand,
+        configListener
+    );
+}
+
+export function deactivate() {
+    if (interval) {
+        clearInterval(interval);
+    }
+}
